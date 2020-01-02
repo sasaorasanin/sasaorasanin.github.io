c82048ac734275e8bf267d237b43201068194ad9
Sasa Orasanin <web@sasaorasanin.com>
2020-01-02 21:41:13 +0100

    css before attr correction

--- projects/scholarships-seekers/js/scripts.js
+++ projects/scholarships-seekers/js/scripts.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
     $.get('https://www.sasaorasanin.com/projects/scholarships-seekers/api.php', function(response) {
         $.each(response, function(i, item) {
+            let website = '';
+            if (item.description) {
+                website = `<a href="${ item.description.includes("http") ? item.description : 'https://' + item.description }">${ item.description }</a>`;
+            }
             $('#data').append(`<tr>
                 <td data-header="#">${ i+1 }</td>
                 <td data-header="Name">${ item.name }</td>
                 <td data-header="State">${ item.state }</td>
                 <td data-header="League">${ item.league }</td>
                 <td data-header="Division">${ item.division }</td>
-                <td data-header="Website">
-                    ${ item.description ? '<a href="' + item.description.includes("http") ? item.description : 'https://' + item.description + '">' + item.description + '</a>' : '-' }
-                </td>
+                <td data-header="Website">${ website }</td>
                 <td data-header="Contacts"><button class="btn btn-info btn-sm float-right">Show</button></td>
             </tr>`);
         });
