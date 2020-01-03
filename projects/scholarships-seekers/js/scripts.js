a9905fa671cb4e159ef3a7b3caf3b2c0e168d329
Sasa Orasanin <web@sasaorasanin.com>
2020-01-03 09:57:08 +0100

    adding about page and fixing icons

--- projects/scholarships-seekers/js/scripts.js
+++ projects/scholarships-seekers/js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    feather.replace();
     $.get('https://www.sasaorasanin.com/projects/scholarships-seekers/api.php', function(response) {
         $.each(response, function(i, item) {
             let website = '';
