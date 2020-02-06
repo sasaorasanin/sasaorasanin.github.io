7433f7bfdb7d7e1cd8d8ce2e7dc0a4e59f33a33d
Sasa Orasanin <49870684+sasaorasanin@users.noreply.github.com>
2020-02-06 21:30:42 +0100

    Update scripts.js

--- projects/scholarships-seekers/js/scripts.js
+++ projects/scholarships-seekers/js/scripts.js
@@ -1,21 +1,9 @@
 $(document).ready(function() {
     $.get('https://www.sasaorasanin.com/projects/scholarships-seekers/api.php', function(response) {
         $.each(response.schools, function(i, item) {
-            let website = '', contacts = '';
+            let website = '';
             if (item.description) {
                 website = `<a target="_blank" href="${ item.description.includes("http") ? item.description : 'https://' + item.description }">${ item.description }</a>`;
-            }
-            if (item.id === response.contacts.id) {
-                contacts = '<a href="#" data-toggle="modal" data-target=".bd-example-modal-xl">Show</a>';
-                $.each(response.contacts.data, function(i, contact) {
-                    $('#contacts').append(`<tr>
-                        <td data-header="#"><span>${ i+1 }</span></td>
-                        <td data-header="Name"><span>${ contact.name }</span></td>
-                        <td data-header="Email"><span><a href="mailto:${ contact.email }">${ contact.email }</a></span></td>
-                        <td data-header="Sport"><span>${ contact.sport }</span></td>
-                        <td data-header="Position"><span>${ contact.position }</span></td>
-                    </tr>`);
-                });
             }
             $('#data').append(`<tr>
                 <td data-header="#"><span>${ i+1 }</span></td>
@@ -24,8 +12,24 @@ $(document).ready(function() {
                 <td data-header="League"><span>${ item.league }</span></td>
                 <td data-header="Division"><span>${ item.division }</span></td>
                 <td data-header="Website"><span>${ website }</span></td>
-                <td data-header="Contacts"><span>${ contacts }</span></td>
+                <td data-header="Contacts"><span><a href="#" class="show-contacts" data-id="${ item.id }">Show</a></span></td>
             </tr>`);
+        });
+    });
+    
+    $(document).on('click','.show-contacts', function() {
+        $.get('https://www.sasaorasanin.com/projects/scholarships-seekers/api.php?id=' + $(this).data("id"), function() {
+            $('#contacts').html('');
+            $.each(response.contacts.data, function(i, contact) {
+                $('#contacts').append(`<tr>
+                    <td data-header="#"><span>${ i+1 }</span></td>
+                    <td data-header="Name"><span>${ contact.name }</span></td>
+                    <td data-header="Email"><span><a href="mailto:${ contact.email }">${ contact.email }</a></span></td>
+                    <td data-header="Sport"><span>${ contact.sport }</span></td>
+                    <td data-header="Position"><span>${ contact.position }</span></td>
+                </tr>`);
+            });
+            $('.bd-example-modal-xl').modal('show');
         });
     });
 });
